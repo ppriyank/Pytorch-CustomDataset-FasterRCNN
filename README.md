3ef258cc4fa844aad481a6d8dfa403721972d044
ppriyank <priyankpathak50@gmail.com>
2019-12-30 20:13:00 -0500

    update2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Pytorch-CustomDataset-FasterRCNN
 Attempt to build Pytorch based FasterRCNN for custom dataset 
+
+
+Tutorial:  [source](https://towardsdatascience.com/faster-r-cnn-object-detection-implemented-by-keras-for-custom-data-from-googles-open-images-125f62b9141a) 
--- model.py
+++ model.py
@@ -13,9 +13,21 @@ class Model(nn.Module):
         resnet50 = torchvision.models.resnet50(pretrained=True)
         resnet50.layer4[0].conv2.stride = (1,1)
         resnet50.layer4[0].downsample[0].stride = (1,1)
+        
+        self.num_anchors = 9        
+        self.feat_dim = 2048
+        self.middle_dim = self.feat_dim  // 4
+
         self.base = nn.Sequential(*list(resnet50.children())[:-2])
 
+        self.rpn_c1 = nn.Conv2d(self.feat_dim, self.middle_dim, 3 , padding=1 )
+        self.relu1 = nn.ReLU()
+
+        self.rpn_cls = nn.Conv2d(self.middle_dim, self.num_anchors, 1 , padding=1 )
+        self.rpn_reg = nn.Conv2d(self.middle_dim, 4* self.num_anchors, 1 , padding=1 )
         
+        self.sigmoid1  = nn.Sigmoid()        
+
     def forward(self, x):
         b = x.size(0)
         h = x.size(2)
@@ -23,7 +35,16 @@ class Model(nn.Module):
 
         x = self.base(x)
         #torch.Size([32, 2048, 50, 38])
+        x = self.relu1(self.rpn_c1(x))
+        #torch.Size([32, 512, 50, 38])
+        # x = torch.rand (1,512,50,38)
+
+        cls_k = self.sigmoid1(self.rpn_cls(x))
+        reg_k = self.rpn_cls(x)
+
         
+        rpn_c1 = nn.Conv2d(self.feat_dim, self.middle_dim, 3 )        
+
         x = F.avg_pool2d(x, x.size()[2:])
         x = x.view(b,t,-1)
         x=x.permute(0,2,1)
